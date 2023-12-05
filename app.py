387923e90530179dc2d3f4ae6f11a700ef55ed6d
Smartucheli <savio.martucheli@gmail.com>
2023-12-05 15:57:02 -0300

    Alterando algumas partes da logica

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import openpyxl 
 from urllib.parse import quote
 import webbrowser
+from time import sleep
+import pyautogui
 
 planilha = openpyxl.load_workbook('planilha_exemplo.xlsx')
 
@@ -12,10 +14,27 @@ for cliente in pagina_numeros.iter_rows(min_row=2):
     telefone = cliente[1].value
     email = cliente[2].value
 
+    if nome is None and telefone is None and email is None:
+        print('Não existe mais contatos processo encerrado')
+        break
+
     mensagem = f'Olá esse é um teste do nosso bot, você {nome} com o telefone {telefone} e email {email} teve a sorte de fazer parte do nosso curso!!!'
 
-    link_mensagem_wpp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+    try:
+        link_mensagem_wpp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
 
-    webbrowser.open(link_mensagem_wpp)
+        webbrowser.open(link_mensagem_wpp)
+        sleep(20)
+        
+        seta = pyautogui.locateCenterOnScreen('seta.png')
+        sleep(2)
 
-    input('')
+        pyautogui.click(seta[0], seta[1])
+        sleep(2)
+
+        pyautogui.hotkey('ctrl', 'w')
+        sleep(3)
+    except:
+        print(f'não enviou mensagem para {nome}')
+        with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
+            arquivo.write(f'{telefone} -- {nome}')
